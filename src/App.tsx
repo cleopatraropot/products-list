e3ea5d0465c607aae4c7629f82105f947ff13915
cleopatraropot <cleopatraropot@gmail.com>
2024-10-25 10:51:15 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import ProductDisplay from "./components/products-display";
 
 function App() {
   return (
     <div className="App">
+      <ProductDisplay/>
     </div>
   );
 }
---
+++ src/components/products-display.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDisplay: React.FC = () => {
+    return (
+        <div>
+            <h2>My products list</h2>
+        </div>
+    )
+}
+
+export default ProductDisplay;
